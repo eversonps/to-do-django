ba0e096cbcda8332697336e4536ace6e5f1a6b6f
Everson Pereira <eversonpereiraofc@gmail.com>
2023-10-30 16:56:18 -0300

    feat: creating todo model

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
+    # apps
     "todo",
+    # lib
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
