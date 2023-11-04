bb29ccddc2984c109e35283ff82759ee6f88cde2
Everson Pereira <eversonpereiraofc@gmail.com>
2023-11-04 18:49:45 -0300

    feat: implementing postgres

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -129,9 +129,13 @@ WSGI_APPLICATION = "todo_api.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_database',
+        'USER': 'everson',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
