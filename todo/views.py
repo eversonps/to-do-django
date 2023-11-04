12ec8bd1b68968fb4eccbaff6a95ef076b58ad79
Everson Pereira <eversonpereiraofc@gmail.com>
2023-11-04 18:09:49 -0300

    feat: associate task to user

--- todo/views.py
+++ todo/views.py
@@ -4,7 +4,7 @@ from todo.serializers import TodoSerializer
 import rest_framework.status as status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, PermissionDenied
 
 from rest_framework.permissions import IsAuthenticated
 
@@ -13,12 +13,15 @@ class TodoListCreate(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        todos = Todo.objects.all()
+        todos = Todo.objects.filter(user=request.user.id)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = TodoSerializer(data=request.data)
+        data = request.data
+        data['user'] = request.user.id
+
+        serializer = TodoSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)       
@@ -30,7 +33,12 @@ class TodoDeleteShowUpdate(APIView):
 
     def get_object(self, pk):
         try:
-            return Todo.objects.get(pk=pk)
+            todo = Todo.objects.get(pk=pk)
+
+            if todo.user_id != self.request.user.id:
+                raise PermissionDenied()
+            
+            return todo
         except Todo.DoesNotExist:
             raise NotFound()
         
@@ -43,7 +51,7 @@ class TodoDeleteShowUpdate(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
     def put(self, request, pk):
-        serializer = TodoSerializer(self.get_object(pk), data=request.data)
+        serializer = TodoSerializer(self.get_object(pk), data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
